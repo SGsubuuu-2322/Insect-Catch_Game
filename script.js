ae743ff5c8c4508436007b8b594e5a8c600fa151
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-01 20:16:57 +0530

    Added the scripts for insect selection and insect creation for the game container...

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ start_btn.addEventListener("click", () => {
   screens[0].classList.add("up");
 });
 
+choose_insect_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const img = btn.querySelector("img");
+    const src = img.getAttribute("src");
+    const alt = img.getAttribute("alt");
+    selected_insect = { src, alt };
+    screens[1].classList.add("up");
+    setTimeout(createInsect, 1000);
+    // satrtGame();
+  });
+});
+
+function createInsect() {
+  const insect = document.createElement("div");
+  insect.classList.add("insect");
+  let { x, y } = getRandomLocation();
+  insect.style.left = `${x}px`;
+  insect.style.top = `${y}px`;
+  insect.innerHTML = `
+  <img src="${selected_insect.src}" alt="${
+    selected_insect.alt
+  }" style="transform: rotate(${Math.random() * 360} deg)">`;
+
+  insect.addEventListener("click", catchInsect);
+  game_container.appendChild(insect);
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+}
+
+function catchInsect() {
+  console.log("123");
+}
