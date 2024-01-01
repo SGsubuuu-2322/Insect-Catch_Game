a29c273e89f51310cb5a86fc1e7941b1f262de32
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-01 19:48:57 +0530

    Added the general scripts for the project and start btn...

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const screens = document.querySelectorAll(".screen");
-// screens[0].classList.add("up");
+const start_btn = document.getElementById("start-btn");
+const choose_insect_btns = document.querySelectorAll(".choose-insect-btn");
+const game_container = document.getElementById("game-container");
+const timeEl = document.getElementById("time");
+const scoreEl = document.getElementById("score");
+const messageEl = document.getElementById("message");
+
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
+
+start_btn.addEventListener("click", () => {
+  screens[0].classList.add("up");
+});
+
